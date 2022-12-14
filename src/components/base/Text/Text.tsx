fe5e4a71a0e25400eab655cf5d061c1488c36a88
Megan <meganlowe@Megans-MBP.localdomain>
2022-12-13 19:17:18 -0800

    Initial commit

---
+++ src/components/base/Text/Text.tsx
@@ -0,0 +1,82 @@
+import React, { useMemo } from "react";
+import clsx from "clsx";
+
+/**
+ * The Text base component is used to present text content with specific,
+ * design-defined typographic styles and scales.
+ *
+ * By default, it will render as an inline `<span>` element, though this can
+ * be overridden by providing a valid elementType via the `as` prop. (such as h1 or h2)
+ *
+ * Text accepts a `variant` prop, which may be used as a shorthand for
+ * rendering text with commonly used combined sets of styles (eg. font-sizes,
+ * weights, families, text-transforms, etc) for different text scales (eg.
+ * for various heading levels or body copy types).
+ *
+ * Additionally, the `transform` prop may be used to apply text-transforms
+ * to the text content.
+ */
+
+export default function Text<T extends React.ElementType = "span">({
+  as: asProp,
+  children,
+  variant,
+  transform,
+}: TextProps<T>) {
+  const As = getAsComponent(asProp, variant);
+
+  // This is a memoized value that will only be recalculated if the
+  // `children` or `transform` props change.
+  // It is used capitalize the text content if the `transform` prop is set to "capitalize"
+  // by using a regex to replace the first letter of each word with an uppercase version.
+  const content = useMemo(() => {
+    if (typeof children === "string" && transform === "capitalize") {
+      return children.replace(/\b\w/g, (l) => l.toUpperCase());
+    }
+
+    return children;
+  }, [children, transform]);
+
+  return (
+    <As
+      className={clsx({
+        uppercase: transform === "uppercase",
+        lowercase: transform === "lowercase",
+
+        "text-[rgb(116,188,68)] text-[40px] font-bold": variant === "heading1",
+        "text-[rgb(116,188,68)] text-[24px] font-bold": variant === "heading2",
+        "text-[rgb(116,188,68)] text-[16px]": variant === "body1",
+        "text-[rgb(116,188,68)] text-[14px]": variant === "body2",
+      })}
+    >
+      {content}
+    </As>
+  );
+}
+
+// This is a helper function that returns the correct element type
+// based on the `as` prop and the `variant` prop.
+// it defaults to `span` if neither are provided.
+const getAsComponent = (asProp?: React.ElementType, variant?: TextVariants) => {
+  const variantToElementTypeOverrideMapping = {
+    heading1: "h1",
+    heading2: "h2",
+  };
+
+  return (
+    asProp ??
+    variantToElementTypeOverrideMapping[
+      variant as keyof typeof variantToElementTypeOverrideMapping
+    ] ??
+    "span"
+  );
+};
+
+export type TextProps<T extends React.ElementType> = {
+  as?: T;
+  children?: React.ReactNode;
+  variant?: TextVariants;
+  transform?: "uppercase" | "lowercase" | "capitalize";
+};
+
+type TextVariants = "heading1" | "heading2" | "body1" | "body2";
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Text from "#components/base/Text";
+
+export default function Home() {
+  return (
+    <>
+      <Text variant="heading1" transform="uppercase">
+        Who We Are
+      </Text>
+
+      <Text variant="heading1">What We Are</Text>
+
+      <Text variant="heading1">Why We Are</Text>
+
+      <Text variant="heading1">When We Are</Text>
+    </>
+  );
+}
