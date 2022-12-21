9d017c77ddd7ce61dfd0adbd158c4a4a75a941ac
Megan <meganlowe@Megans-MBP.localdomain>
2022-12-21 13:42:43 -0800

    added programs to home page
    added two columns for horizon, meridian, and comp. all changes in pages -> index

---
+++ src/components/partial/AppBodyPartial/AppBodyPartial.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Text from "#components/base/Text";
+import Link from "next/link";
+
+export default function AppBodyPartial() {
+  return (
+    <div class="relative columns-1 sm:columns-2">
+      <img class="w-full" src="horizonprogram.jpeg" />
+    </div>
+  );
+}
