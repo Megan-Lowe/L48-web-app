debdd26e78b42ecfc9f89db378fb9820d65e0ba3
Megan <meganlowe@Megans-MBP.localdomain>
2022-12-29 23:55:22 -0800

    Update ContactGridPartial.tsx
    centered, added social media buttons

--- src/components/partial/ContactGridPartial/ContactGridPartial.tsx
+++ src/components/partial/ContactGridPartial/ContactGridPartial.tsx
@@ -1,14 +1,17 @@
 import Text from "#components/base/Text";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function ContactGridPartial() {
   return (
     <>
       <div
-        className="mx-auto bg-black p-5 basis-1/2"
+        className="mx-auto bg-black p-5 basis-1/2 text-center"
         style={{ gridArea: "Contact" }}
       >
-        <Text colour="white" className="text-3xl font-sans text-center">
+        <Text colour="white" className="text-3xl font-sans">
           CONTACT LATITUDE 48 <br /> PADDLING CLUB
+          <br />
           <br />
         </Text>
         <Text className="text-center text-sm">
@@ -16,6 +19,32 @@ export default function ContactGridPartial() {
           <br /> <br />
           latitude48paddling@gmail.com
         </Text>
+        <br />
+        <br />
+        <div className="flex px-5 justify-center">
+          <Link
+            href="https://www.facebook.com/Latitude-48-Paddling-Cold-Water-Ohana-641255982653985/"
+            target="_blank"
+          >
+            <Image
+              src="/images/facebook.png"
+              height={30}
+              width={30}
+              alt="facebooklogo"
+            />
+          </Link>
+          <Link
+            href="https://www.instagram.com/latitude48paddlingclub/"
+            target="_blank"
+          >
+            <Image
+              src="/images/instagram.png"
+              height={30}
+              width={30}
+              alt="instagram logo"
+            />
+          </Link>
+        </div>
       </div>
       <div
         className="mx-auto bg-black p-5 basis-1/2"
