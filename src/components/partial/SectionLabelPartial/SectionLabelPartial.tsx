3874c70b7ae90fa14d1d9c402ba1a17fa382a756
Megan <meganlowe@Megans-MacBook-Pro.local>
2022-12-30 13:36:10 -0800

    attempted text overlay
    tried to add text overlay for section label partial

--- src/components/partial/SectionLabelPartial/SectionLabelPartial.tsx
+++ src/components/partial/SectionLabelPartial/SectionLabelPartial.tsx
@@ -2,12 +2,13 @@ import Text from "#components/base/Text";
 
 export default function SectionLabelPartial() {
   return (
-    <div>
+    <div className="image-container">
       <img
         className="w-full basis-1/2"
         src="/images/span.jpeg"
         alt="a greyscale image of misty mountains"
       />
+      <div className="image-text">Text Overlay</div>
     </div>
   );
 }
